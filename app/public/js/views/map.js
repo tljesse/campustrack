93148dca83b9d58b60da413d5bb88abe1a596c80
Tristan Jesse <Tristan Jesse>
2016-04-27 16:21:36 -0500

    added nexmo inbound

--- app/public/js/views/map.js
+++ app/public/js/views/map.js
@@ -6,6 +6,47 @@ var map = L.mapbox.map('map', 'mapbox.streets')
 var myLayer = L.mapbox.featureLayer().addTo(map);
 var geoJson = [];
 
+var http = require('http')
+var url = require('url')
+
+// create the http server
+http.createServer(function (request, response) {
+
+  if(request.method=='POST') {
+      //Do something for post request
+  }
+  else if(request.method=='GET') {
+      //Check that this is a delivery receipt
+      var url_parts = url.parse(request.url,true);
+
+      if (!url_parts.hasOwnProperty('to') || !url_parts.hasOwnProperty('msisdn') || !url_parts.hasOwnProperty('text'))
+        console.log('This is not an inbound message');
+      else {
+        //This is a DLR, check that your message has been delivered correctly
+        if (url_parts.hasOwnProperty('concat'))
+        {
+          console.log("Fail:" +  url_parts.status + ": " + url_parts.err-code  +  ".\n" );
+        }
+        else {
+          console.log("Success");
+          /*
+           * The following parameters in the delivery receipt should match the ones
+           * in your request:
+           * Request - from, dlr - to\n
+           * Response - message-id, dlr - messageId\n
+           * Request - to, Responese - to, dlr - msisdn\n
+           * Request - client-ref, dlr - client-ref\n
+           */
+        }
+
+      }
+  }
+  //Send the 200 ok to the Platform so you don't get sent the DLR again.
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.end();
+
+}).listen(80);
+
 if (typeof(udata) != 'undefined'){
   if(navigator.geolocation) {
     map.locate();
