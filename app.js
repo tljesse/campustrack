87a4c00f10d7bf2a54ebcccf73b80d2f8816b24b
Tristan Jesse <Tristan Jesse>
2016-04-28 01:36:04 -0500

    update app.js

--- app.js
+++ app.js
@@ -37,20 +37,6 @@ require('./app/server/routes')(app);
 if (app.get('env') == 'development') app.use(errorHandler());
 
 
-http.createServer(app).listen(app.get('port'), function(request, response){
+http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
-
-	var headers = request.headers;
-	var method = request.method;
-	var url = request.url;
-	var body = [];
-
-	request.on('error', function(err) {
-		console.error(err);
-	}).on('data', function(chunk) {
-		body.push(chunk);
-	}).on('end', function() {
-		body = Buffer.concat(body).toString();
-	})
-
 });
