c65abbfc14be7594642e616bca1df1c64261d297
Tristan Jesse <Tristan Jesse>
2016-04-27 21:49:00 -0500

    edit app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@
 **/
 
 var http = require('http');
-var url = require('url');
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
@@ -38,39 +37,6 @@ require('./app/server/routes')(app);
 if (app.get('env') == 'development') app.use(errorHandler());
 
 
-http.createServer(app).listen(app.get('port'), function(request, response){
+http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
-
-	if(request.method=='POST') {
-      //Do something for post request
-  }
-  else if(request.method=='GET') {
-      //Check that this is a delivery receipt
-      var url_parts = url.parse(request.url,true);
-
-      if (!url_parts.hasOwnProperty('to') || !url_parts.hasOwnProperty('msisdn') || !url_parts.hasOwnProperty('text'))
-        console.log('This is not an inbound message');
-      else {
-        //This is a DLR, check that your message has been delivered correctly
-        if (url_parts.hasOwnProperty('concat'))
-        {
-          console.log("Fail:" +  url_parts.status + ": " + url_parts.err-code  +  ".\n" );
-        }
-        else {
-          console.log("Success");
-          /*
-           * The following parameters in the delivery receipt should match the ones
-           * in your request:
-           * Request - from, dlr - to\n
-           * Response - message-id, dlr - messageId\n
-           * Request - to, Responese - to, dlr - msisdn\n
-           * Request - client-ref, dlr - client-ref\n
-           */
-        }
-
-      }
-  }
-  //Send the 200 ok to the Platform so you don't get sent the DLR again.
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.end();
 });
